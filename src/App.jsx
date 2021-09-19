fb4b75aeb28a0ba88acaccdc0f20c41db7153ac0
Carlos Roberto <carlosrobertounopar01@hotmail.com>
2021-09-19 01:20:48 -0300

    config ambiente dev e production

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 
 export function App(){
-    return <h1> wello word</h1>
+    
+    return <h1> wello word test 1,2,3</h1>
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 
-
-
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-    mode:'development',
+    mode: isDevelopment ? 'development' :'production' ,
+    devtool:isDevelopment ? 'eval-source-map' : 'source-map',
     entry: path.resolve(__dirname, 'src', 'index.jsx'),
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -14,6 +14,9 @@ module.exports = {
     },
     resolve: {
         extensions: ['.js', '.jsx']
+    },
+    devServer:{
+        static: path.resolve(__dirname,'public')
     },
     plugins: [
        
